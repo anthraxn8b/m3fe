bd99c0030225ee3b8007f4106aa270096d14efaa
anthraxn8b <boris@kairat.eu>
2015-06-03 21:57:10 +0200

    Initial m3 frontend commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name m3feApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the m3feApp
+ */
+angular.module('m3feApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name m3feApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the m3feApp
+ */
+angular.module('m3feApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
